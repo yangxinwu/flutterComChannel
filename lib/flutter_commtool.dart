cd3bdb392841e20369b5caa3b7a048519c852ae0
Bevan <xianwei.yan@quvideo.com>
2019-09-20 11:56:22 +0800

    update

--- lib/flutter_commtool.dart
+++ lib/flutter_commtool.dart
@@ -6,38 +6,35 @@ class FlutterCommtool {
   static const MethodChannel _channel = const MethodChannel('flutter_commtool');
 
   static Future<String> getString(String name) async {
-    final String data = await _channel.invokeMethod('getString', name);
-    return data;
+    return await _channel.invokeMethod('getString', name);
   }
 
   static Future<Map<String, String>> getStrings(List<String> names) async {
-    final data =
-    await _channel.invokeMethod('getStrings', names);
+    final data = await _channel.invokeMethod('getStrings', names);
     return Map<String, String>.from(data);
   }
 
   static Future<dynamic> execute(name) async {
-    final data = await _channel.invokeMethod('execute', name);
-    return data;
+    return await _channel.invokeMethod('execute', name);
   }
 
   static Future<String> goWeb(String tag) async {
-    final String data = await _channel.invokeMethod('goWeb', tag);
-    return data;
+    return await _channel.invokeMethod('goWeb', tag);
   }
 
   static Future<String> goPage(String page) async {
-    final String data = await _channel.invokeMethod('goPage', page);
-    return data;
+    return await _channel.invokeMethod('goPage', page);
   }
 
   static Future<String> get(Map<String, String> map) async {
-    final String data = await _channel.invokeMethod('get', map);
-    return data;
+    return await _channel.invokeMethod('get', map);
   }
 
   static Future<String> post(Map<String, String> map) async {
-    final String data = await _channel.invokeMethod('post', map);
-    return data;
+    return await _channel.invokeMethod('post', map);
+  }
+
+  static Future<dynamic> cancel(String tag) async {
+    return await _channel.invokeMethod('cancel', tag);
   }
 }
